5413bd1e162a02ad7ce6d6f9c9f5e55486b3bc89
Bene Koehler <benedikt.koehler@peerigon.com>
2021-12-29 13:42:48 +0100

    test

--- src/main.ts
+++ src/main.ts
@@ -3,26 +3,42 @@ import * as github from "@actions/github";
 import axios from "axios";
 
 async function run(): Promise<void> {
+  core.debug("start of action");
+
   try {
     const hookUrl = core.getInput("mattermost-hook-url");
     const repoToken = core.getInput("repo-token");
+
+    const octokit = github.getOctokit(repoToken);
+    const test = await octokit.rest.repos.getCommit({
+      owner: github.context.payload.sender?.login,
+      ref: github.context.sha,
+      repo: github.context.payload.repository?.name ?? "",
+    });
+
+    core.debug(JSON.stringify(test, null, 2));
+
     const headers = {Authorization: `Bearer ${repoToken}`};
 
     const {payload} = github.context;
+    core.debug(`commit that triggered the action: ${github.context.sha}`);
+
     const compareUrlRaw = payload.repository?.compare_url.replace(
       payload.repository.name,
       `${payload.repository.name}.wiki`,
     );
+    core.debug(`compare url raw: ${compareUrlRaw}`);
 
     const commitsUrl = payload.repository?.commits_url
       .replace(payload.repository.name, `${payload.repository.name}.wiki`)
       .replace("{/sha}", "?per_page=2");
 
+    core.debug(`Commits URL: ${commitsUrl}`);
+
     const {data: commits} = await axios.get<Array<any>>(commitsUrl, {
       headers,
     });
 
-    core.debug(`Commits URL: ${commitsUrl}`);
     core.debug(`Commits: \n${commits.join("\n ")}`);
 
     if (commits.length >= 2) {
